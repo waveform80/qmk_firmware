e44065674b443f5610a960e7a53e2968ae565bf4
Dave Jones <dave@waveform.org.uk>
2018-10-08 10:05:14 +0100

    Backspace on right thumb

--- layouts/community/ergodox/dave/keymap.c
+++ layouts/community/ergodox/dave/keymap.c
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | Esc    |   1  |   2  |   3  |   4  |   5  |   6  |           |   6  |   7  |   8  |   9  |   0  |   -  | BkSpce |
+ * | Esc    |   1  |   2  |   3  |   4  |   5  |   6  |           |   6  |   7  |   8  |   9  |   0  |   -  |   =    |
  * |--------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |   Y  |           |   G  |   Y  |   U  |   I  |   O  |   P  | Enter  |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        | PrtS | PrtS |       | CtAl | CtAl |
  *                                 ,------+------+------|       |------+------+------.
  *                                 |      |      |  L1  |       |  L1  |      |      |
- *                                 | Spce | ~L2  +------|       |------+  ~L1 | Spce |
+ *                                 | Spce | ~L2  +------|       |------+  ~L1 | BkSp |
  *                                 |      |      | LGui |       | RGui |      |      |
  *                                 `--------------------'       `--------------------'
  */
@@ -48,14 +48,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                               TO(PROG),
                                             KC_SPC, MO(NAVI), KC_LGUI,
         // right hand
-             KC_6,    KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_BSPC,
+             KC_6,    KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQUAL,
              KC_G,    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,    KC_ENTER,
                       KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN, KC_ENTER,
              KC_B,    KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH, KC_RSFT,
                               KC_RALT,KC_LBRC,KC_RBRC,KC_NO,   CTL_T(KC_QUOT),
              MT(0x5, KC_NO), MT(0x5, KC_NO),
              TO(PROG),
-             KC_RGUI, MO(PROG), KC_SPC
+             KC_RGUI, MO(PROG), KC_BSPC
     ),
 
 /* Keymap 1: Symbol Layer
